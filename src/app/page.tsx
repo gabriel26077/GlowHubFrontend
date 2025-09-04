ed291e6015668324d032db8c52cbcc3ded02cb7b
gabriel26077 <gabrielsebastiao321@gmail.com>
2025-09-04 14:52:22 -0300

    feat: setup framework

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div style={{backgroundColor: 'red', height: '100vh'}}>
+      
+
+
+
+      AAAAAAAAAA
+    </div>
+  );
+}
